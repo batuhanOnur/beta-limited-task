87aa5c3d21b03bcbc5db1705f58dbb6e1f30d35b
batuhan onur <batuhanonur29@gmail.com>
2023-11-25 22:51:00 +0300

    bug fix

--- src/components/Header/contents/HeaderIcons.tsx
+++ src/components/Header/contents/HeaderIcons.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import Badge from "@mui/material/Badge"
-import Box from "@mui/material/Box"
+import Box from '@mui/material/Box'
 import Stack from "@mui/material/Stack";
 import LocalMallIcon from '@mui/icons-material/LocalMall';
 import PersonIcon from '@mui/icons-material/Person';
@@ -34,25 +34,25 @@ const HeaderIcons = () => {
 
     const badgeContent = useCartQuantity()
 
-  return (
-    <Stack spacing={2} direction="row" justifyContent='center'>
-        <Badge color="secondary" invisible={false}>
-            <Circle>
-                <PersonIcon style={{
-                    color: '#858686'
-                }} />
-            </Circle>
-        </Badge> 
-
-        <Badge color="error" badgeContent={badgeContent ?? 0}>
-            <Circle>
-                <LocalMallIcon style={{
-                    color: '#858686'
-                }}/>
-            </Circle>
-        </Badge>        
-    </Stack>
-  )
+    return (
+        <Stack spacing={2} direction="row" justifyContent='center'>
+            <Badge color="secondary" invisible={false}>
+                <Circle>
+                    <PersonIcon style={{
+                        color: '#858686'
+                    }} />
+                </Circle>
+            </Badge> 
+            
+            <Badge color="error" badgeContent={badgeContent ?? 0}>
+                <Circle>
+                    <LocalMallIcon style={{
+                        color: '#858686'
+                    }}/>
+                </Circle>
+            </Badge> 
+        </Stack>
+    )
 }
 
 export default HeaderIcons
