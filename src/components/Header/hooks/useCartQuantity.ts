fa483b97a5ed2cd4cae790653763db7627466d61
batuhan onur <batuhanonur29@gmail.com>
2023-11-25 22:51:07 +0300

    bug fix

--- src/components/Header/hooks/useCartQuantity.ts
+++ src/components/Header/hooks/useCartQuantity.ts
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 
 
 const useCartQuantity = () => {
-     
+    
     const sessionId = useSessionId()
     const [cartQuery,result] = useLazyGetCartProductsQuery()
     
